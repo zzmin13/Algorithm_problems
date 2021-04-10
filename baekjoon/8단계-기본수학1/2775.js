f3df3e7e8cc9623777d8afc220b977039204bfca
zzmin13 <zzzz1997@naver.com>
2021-04-10 17:02:11 +0900

    baekjoon - 8. 기본수학1 2839.js

--- baekjoon/8단계-기본수학1/2775.js
+++ baekjoon/8단계-기본수학1/2775.js
@@ -46,25 +46,25 @@ for(let i = 0; i < number*2 ; i=i+2){
     array.push([k,n]);
 }
 
-let lowerFloor = [];
+let floor = [];
 let sum = 0;
 for(let i = 0; i < array.length; i++){
     //k층 n호
     k = array[i][0];
     n = array[i][1];
     for(let j = 0; j <= k; j++){ //0층부터 k층까지
-        lowerFloor[j] = [];
+        floor[j] = [];
         for(let m = 1; m <= n; m++){ // 1호부터 n호까지
             if(j === 0){ //0층이라면
-                lowerFloor[j].push(m);
+                floor[j].push(m);
             }else{ // j가 0이 아닐 때 
-                sum += lowerFloor[j-1][m-1];
-                lowerFloor[j].push(sum);
+                sum += floor[j-1][m-1];
+                floor[j].push(sum);
                 if(m === n){
                     sum = 0;
                 }
             }
         }
     }
-    console.log(lowerFloor[k][n-1]);
+    console.log(floor[k][n-1]);
 }
