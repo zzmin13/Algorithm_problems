1d8b6b2ac60c915aa25916d3f7f6880e202ae46e
zzmin13 <zzzz1997@naver.com>
2021-04-23 17:26:19 +0900

    baekjoon - 10. 재귀함수 11729.js

--- baekjoon/10단계-재귀함수/11729.js
+++ baekjoon/10단계-재귀함수/11729.js
@@ -38,22 +38,24 @@ let input = fs.readFileSync("예제.txt").toString();
 
 let N = Number(input); // 원판의 갯수
 let count = 0;
+let answer = [];
 // num : 원반의 개수
 // from : 출발지 기둥 번호
 // to : 목적지 기둥 번호
 // other : 나머지 기둥 번호
-function Hanoi(num, from, to, other){
-    console.log(`num: ${num} , from : ${from}, to : ${to}, other : ${other}`);
+function Hanoi(num, from, other, to){
     if(num === 0) {
         return;
+    }else{
+        // 첫번째 호출에서는 받아온 원반 갯수보다 하나 적은 원반들을 목적지가 아닌 곳으로 재귀적으로 이동
+        Hanoi(num - 1 , from, to, other);
+        // 맨 아래 원반을 목적지로 이동시킴
+        answer.push([from, to]);
+        count++;
+        //다른 곳으로 옮겼던 원반들을 그 위에 얹음
+        Hanoi(num - 1, other, from, to);
     }
-    // 첫번째 호출에서는 받아온 원반 갯수보다 하나 적은 원반들을 목적지가 아닌 곳으로 재귀적으로 이동
-    Hanoi(num - 1 , from, other, to);
-    // 맨 아래 원반을 목적지로 이동시킴
-    console.log(`${from}번에서 ${to}로 옮긴다.`);
-    count++;
-    //다른 곳으로 옮겼던 원반들을 그 위에 얹음
-    Hanoi(num - 1, other, to, from);
 }
-Hanoi(1, '1','3','2');
+Hanoi(N, '1','2','3');
 console.log(count);
+console.log(answer.map((element) => element.join(" ")).join("\n"));
