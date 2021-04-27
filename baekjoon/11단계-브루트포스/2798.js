bab2fdfd5826d551fe0ca46ddf5aac4c491aac3f
zzmin13 <zzzz1997@naver.com>
2021-04-27 09:53:46 +0900

    baekjoon - 11.브루트포스 2798.js

--- baekjoon/11단계-브루트포스/2798.js
+++ baekjoon/11단계-브루트포스/2798.js
@@ -41,7 +41,27 @@ input[0] = input[0].split(" ");
 const cards = input[1].split(" ").map((element) => Number(element));
 const N = Number(input[0][0]); // N은 총 카드의 개수
 const M = Number(input[0][1]); // M은 3개의 숫자의 합과 가장 가까운 목표 값
-console.log(input[1]);
-for(let i = 0; i < cards.length; i++){
-   console.log(cards[i]);
+let answer = 0;
+
+const getCombinations = (array, selectNumber) => {
+   const results = [];
+   if(selectNumber === 1){
+      return array.map((element) => [element]);
+   }
+   array.forEach((fixed, index, origin) => {
+      const rest = origin.slice(index + 1);
+      const combinations = getCombinations(rest, selectNumber - 1);
+      const attached = combinations.map((combination) => [fixed, ...combination]);
+      results.push(...attached);
+   });
+   return results;
 }
+
+getCombinations(cards, 3).forEach((element) => {
+   let sum = element[0] + element[1] + element[2];
+   let gap = M - sum;
+   if(gap >= 0 && answer <= sum){
+      answer = sum;
+   }
+});
+console.log(answer);
