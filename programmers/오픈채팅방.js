89610531568e0f5fe889b4f02bec5d4c971fdaf1
zzmin13 <zzzz1997@naver.com>
2021-06-24 13:34:19 +0900

    programmers - 오픈채팅방.js

---
+++ programmers/오픈채팅방.js
@@ -0,0 +1,32 @@
+function solution(record) {
+  const answer = [];
+  const map = new Map();
+  for (let i = 0; i < record.length; i++) {
+    const target = record[i].split(" ");
+    // target[0] = Enter, Leave, Change
+    // target[1] = uid
+    // target[2] = name
+    if (target[0] === "Enter" || target[0] === "Change") {
+      map.set(target[1], target[2]);
+    }
+  }
+  for (let i = 0; i < record.length; i++) {
+    const target = record[i].split(" ");
+    if (target[0] === "Enter") {
+      const string = map.get(target[1]) + "님이 들어왔습니다.";
+      answer.push(string);
+    } else if (target[0] === "Leave") {
+      const string = map.get(target[1]) + "님이 나갔습니다.";
+      answer.push(string);
+    }
+  }
+  return answer;
+}
+const record = [
+  "Enter uid1234 Muzi",
+  "Enter uid4567 Prodo",
+  "Leave uid1234",
+  "Enter uid1234 Prodo",
+  "Change uid4567 Ryan",
+];
+console.log(solution(record));
